c7d7eab00e3dcf413312ca0cf95632b8811047ac
vk <vk@vk.com>
2024-04-23 22:25:23 +0530

    added remove dupicated

--- remove dublicate sorted array.py
+++ remove dublicate sorted array.py
@@ -1,15 +1,25 @@
 class Solution:
     def removeDuplicates(self, nums: list[int]) -> int:
-        s1=set()
-        for i in nums:
-            if i not in s1:
-                s1.add(i)
-        n=list(s1)
-        for i in range(len(n),len(nums)):
-            n.append('_')
-        nums=n
+        if not nums:
+            return 0
+
+        k = 0
+        for i in range(1, len(nums)):
+            if nums[i] != nums[k]:
+                k += 1
+                nums[k] = nums[i]
         print(nums)
-        return len(s1)
+        return k + 1
+        # s1=set()
+        # for i in nums:
+        #     if i not in s1:
+        #         s1.add(i)
+        # n=list(s1)
+        # for i in range(len(n),len(nums)):
+        #     n.append('_')
+        # nums=n
+        # print(nums)
+        # return len(s1)
 
 s=Solution()
 li=[1,1,2]
